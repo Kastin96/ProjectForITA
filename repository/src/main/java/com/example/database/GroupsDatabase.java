c3895a67e3d74d6617f6fe353c91b6bb3c55afdb
Kastin <Kastin96@gmail.com>
2020-12-26 14:16:48 +0300

    Added Groups и database for Groups

---
+++ repository/src/main/java/com/example/database/GroupsDatabase.java
@@ -0,0 +1,23 @@
+package com.example.database;
+
+import com.example.groups.Group;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class GroupsDatabase {
+
+    private static volatile Map<UUID, Group> instance;
+
+    public static Map<UUID, Group> getInstance() {
+        if (instance == null) {
+            synchronized (GroupsDatabase.class) {
+                if (instance == null) {
+                    instance = new HashMap<>();
+                }
+            }
+        }
+        return instance;
+    }
+}
