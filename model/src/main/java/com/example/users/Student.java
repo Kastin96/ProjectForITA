606ab152d286053aca1712624124a9aac183296c
Kastin <Kastin96@gmail.com>
2020-12-26 14:12:26 +0300

    Added empty Constructor

--- model/src/main/java/com/example/users/Student.java
+++ model/src/main/java/com/example/users/Student.java
@@ -1,6 +1,10 @@
 package com.example.users;
 
 public class Student extends User {
+
+    public Student() {
+    }
+
     public Student(String login, String password, String fullName, int age) {
         super(login, password, fullName, age);
     }
