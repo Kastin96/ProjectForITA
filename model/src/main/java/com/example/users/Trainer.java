03dd93c02aea15213bbc03c456d0011f230854c8
Kastin <Kastin96@gmail.com>
2020-12-26 14:11:54 +0300

    Added Getter/Setter and Constructor

--- model/src/main/java/com/example/users/Trainer.java
+++ model/src/main/java/com/example/users/Trainer.java
@@ -7,4 +7,23 @@ public class Trainer extends User {
 
     private List<Integer> salaryList = new LinkedList<>();
 
+    public Trainer() {
+    }
+
+    public Trainer(String login, String password, String fullName, int age, List<Integer> salaryList) {
+        super(login, password, fullName, age);
+        this.salaryList = salaryList;
+    }
+
+    public void addSalary(int salary) {
+        this.salaryList.add(salary);
+    }
+
+    public List<Integer> getSalaryList() {
+        return salaryList;
+    }
+
+    public void setSalaryList(List<Integer> salaryList) {
+        this.salaryList = salaryList;
+    }
 }
