716c551bd27872d18913be5bcb1f2221439d2a79
Kastin <Kastin96@gmail.com>
2020-12-27 12:18:15 +0300

    Deleted else condition

--- controller/src/main/java/com/example/controllers/salary/AverageSalaryController.java
+++ controller/src/main/java/com/example/controllers/salary/AverageSalaryController.java
@@ -1,11 +1,9 @@
 package com.example.controllers.salary;
 
-import com.example.database.UserDatabase;
 import com.example.salary.AverageSalary;
 import com.example.users.Trainer;
-import com.example.users.User;
 
-import javax.lang.model.element.ModuleElement;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,8 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.UUID;
 
 @WebServlet(urlPatterns = "/countaveragesalary")
 public class AverageSalaryController extends HttpServlet {
@@ -25,11 +21,10 @@ public class AverageSalaryController extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         Object user = session.getAttribute("user");
+
         if (user instanceof Trainer){
             BigDecimal averageSalary = AverageSalary.getCount(((Trainer) user).getSalaryList());
             session.setAttribute("averageSalary", averageSalary);
-        } else {
-            session.setAttribute("averageSalary", "Average salary is not available!");
         }
 
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/salarylist");
