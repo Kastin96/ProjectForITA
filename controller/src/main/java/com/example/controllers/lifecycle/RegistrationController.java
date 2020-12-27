54cedc400bf7aa7e3747e691693e16a3944ed673
Kastin <Kastin96@gmail.com>
2020-12-27 12:16:03 +0300

    Added check for login

--- controller/src/main/java/com/example/controllers/lifecycle/RegistrationController.java
+++ controller/src/main/java/com/example/controllers/lifecycle/RegistrationController.java
@@ -1,12 +1,9 @@
 package com.example.controllers.lifecycle;
 
 import com.example.database.UserDatabase;
-import com.example.hardcoremetod.HardcoreMethod;
+import com.example.search.SearchFromDatabase;
 import com.example.users.Student;
-import com.example.users.Trainer;
-import com.example.users.User;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,8 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Date;
 
 
 @WebServlet(urlPatterns = "/registration")
@@ -32,12 +27,18 @@ public class RegistrationController extends HttpServlet {
         try {
             int age = Integer.parseInt(req.getParameter("age"));
 
-            Student student = new Student(login, password, name, age);
-            UserDatabase.getInstance().put(student.getId(), student);
+            if (SearchFromDatabase.findUserFromUserDatabase(login) == null) {
+                Student student = new Student(login, password, name, age);
+                UserDatabase.getInstance().put(student.getId(), student);
 
-            session.setAttribute("goodRegistration", "Registration was successful!");
+                session.setAttribute("goodRegistration", "Registration was successful!");
 
-            resp.sendRedirect("/new/signin");
+                resp.sendRedirect("/new/signin");
+            } else {
+                session.setAttribute("badRegistration", "This login is already in use!");
+
+                resp.sendRedirect("/new/reg");
+            }
         } catch (NumberFormatException e) {
             session.setAttribute("badRegistration", "Age error: Number must be entered!");
 
