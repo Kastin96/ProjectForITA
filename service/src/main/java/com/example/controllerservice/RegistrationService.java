f703534cdbb9670c676b5febbd3c276ce0f7d02a
Kastin <Kastin96@gmail.com>
2021-01-23 00:52:34 +0300

    Added Service to Authentication and Registration

--- service/src/main/java/com/example/controllerservice/RegistrationService.java
+++ service/src/main/java/com/example/controllerservice/RegistrationService.java
@@ -1,4 +1,15 @@
 package com.example.controllerservice;
 
+import com.example.database.StudentRepositoryPostgres;
+import com.example.users.Student;
+
 public class RegistrationService {
+
+    public static boolean registration(String login, String password, String fullName, Integer age){
+        return StudentRepositoryPostgres.getInstance().save(new Student()
+                .withLogin(login)
+                .withPassword(password)
+                .withFullName(fullName)
+                .withAge(age));
+    }
 }
