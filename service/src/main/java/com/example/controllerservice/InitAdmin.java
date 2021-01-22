a3eee22231028fd4c0b57e980f68bccc78dfc299
Kastin <Kastin96@gmail.com>
2021-01-23 00:46:04 +0300

    Create class for init Admin from InitParameter

---
+++ service/src/main/java/com/example/controllerservice/InitAdmin.java
@@ -0,0 +1,23 @@
+package com.example.controllerservice;
+
+import com.example.database.AdministratorRepositoryPostgres;
+import com.example.users.Administrator;
+
+public class InitAdmin {
+
+    public static void init(String login, String password) {
+        AdministratorRepositoryPostgres.getInstance()
+                .save(new Administrator()
+                        .withLogin(login)
+                        .withPassword(password));
+    }
+
+    public static void remove(String login) {
+        AdministratorRepositoryPostgres.getInstance()
+                .remove(AdministratorRepositoryPostgres
+                        .getInstance()
+                        .getPersonByLogin(login)
+                        .get()
+                        .getId());
+    }
+}
