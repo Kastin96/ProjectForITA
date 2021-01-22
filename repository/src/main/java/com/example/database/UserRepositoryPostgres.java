872f23b3f35ce110c62f19dbc1038383a1a806b8
Kastin <Kastin96@gmail.com>
2021-01-23 00:46:43 +0300

    Create class to check and push user type

---
+++ repository/src/main/java/com/example/database/UserRepositoryPostgres.java
@@ -0,0 +1,54 @@
+package com.example.database;
+
+import com.example.users.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.*;
+
+public class UserRepositoryPostgres {
+    Logger log = LoggerFactory.getLogger(UserRepositoryPostgres.class);
+
+    private final String SELECT_ROLE_FROM_USERS_BY_LOGIN = "SELECT users.id, users.login, roles.role FROM users \n" +
+            "INNER JOIN roles ON users.role = roles.id \n" +
+            "WHERE users.login = ?;";
+
+    private volatile static UserRepositoryPostgres instance;
+
+    public static UserRepositoryPostgres getInstance() {
+        if (instance == null) {
+            synchronized (UserRepositoryPostgres.class) {
+                if (instance == null) {
+                    instance = new UserRepositoryPostgres();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Optional<? extends User> getUserByLogin(String login) {
+        try (Connection con = DataSource.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(SELECT_ROLE_FROM_USERS_BY_LOGIN)) {
+            ps.setString(1, login);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                final String userRole = rs.getString("role");
+                final int userId = rs.getInt("id");
+
+                if (userRole.equalsIgnoreCase(Roles.ADMIN.getRole())) {
+                    return AdministratorRepositoryPostgres.getInstance().find(userId);
+                } else if (userRole.equalsIgnoreCase(Roles.TRAINER.getRole())) {
+                    return TrainerRepositoryPostgres.getInstance().find(userId);
+                } else {
+                    return StudentRepositoryPostgres.getInstance().find(userId);
+                }
+            }
+
+        } catch (SQLException sqlException) {
+            log.error("Error checking User role from database");
+        }
+        return Optional.empty();
+    }
+}
