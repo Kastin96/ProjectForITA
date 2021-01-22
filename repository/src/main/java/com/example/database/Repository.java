1706dfc03e66f45dc8b60332e12c1105fe318aa9
Kastin <Kastin96@gmail.com>
2021-01-22 21:04:30 +0300

    Inject some correct in DAO

---
+++ repository/src/main/java/com/example/database/Repository.java
@@ -0,0 +1,16 @@
+package com.example.database;
+
+import com.example.users.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T> {
+
+    Optional<T> find(Integer id);
+    List<T> findAll();
+    boolean save(T entity);
+//    boolean update(T entity);
+    boolean remove(Integer id);
+
+}
